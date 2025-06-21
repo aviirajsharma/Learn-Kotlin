3bf5cb4f9f7b863a6a1174abd1b1793a2c8ed95e
Aviraj Sharma <avirajkanhaua0413@gmail.com>
2025-06-21 23:54:21 +0530

    first commit

---
+++ app/src/main/java/com/cscorner/learn_kotlin/1_Basics_of_Kotlin/1_Kotlin_Intro.kt
@@ -0,0 +1,39 @@
+package com.cscorner.learn_kotlin
+
+/*
+        Kotlin was created by the jetbrains in the 2010-11 and the first version released in 2016.
+        Kotlin compiles to JVM bytecode -> Runs on the Java Virtual Machine (JVM).
+        Kotlin code is converted to Java bytecode -> Executed by the JVM.
+
+
+
+
+        Why JetBrains created Kotlin?
+
+             Jetbrain needed a modern language that:
+             Was interoperable with Java.
+             Had better syntax (less boilerplate).
+             Was type-safe (avioding NullPointerExceptions).
+             Had first-class support for functional programming.
+
+        Since existing languages like Scala, Groovy and Clojure had issue  (slow compilation, complexity),
+        JetBrains decided to create thier own language called Kotlin.
+
+
+        Why did Google choose Kotlin as the official languauge? (accepted official langauage in 2017)
+
+        Ans->>
+             Java had limitations -> Kotlin solved them with better syntax and safety.
+             Kotlin was 100% interoperable with Java. -> No need to rewrite code.
+             JetBrains actively supports Kotlin -> Frequent updates & improvements.
+             Kotlin had massive developer demand -> Many Android developers preferred Kotlin over Java.
+             Google wanted a modern, efficient language for Android development. -> Kotlin has better performance and fewer crashes.
+
+
+        How popular it is right now and its scope->
+
+             Kotlin is already used by over 70% professional Android Developers,
+             and 95% of top 1,000 Android Apps contains Kotlin code.
+
+
+*/
